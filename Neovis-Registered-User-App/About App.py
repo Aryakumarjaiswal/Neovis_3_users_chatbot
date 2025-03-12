48c680e912ada5ac4119de812dcd57f6f45356b2
Aryakumar Jaiswal <86922289+Aryakumarjaiswal@users.noreply.github.com>
2025-03-12 08:52:22 +0530

    Update About App.py
    Updated names

--- Neovis-Registered-User-App/About App.py
+++ Neovis-Registered-User-App/About App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Set the title of the app
-st.set_page_config(page_title="Welcome to Hotel Management App", page_icon="🏨", layout="centered")
+st.set_page_config(page_title="Neovis external bot assistant", page_icon="🏨", layout="centered")
 
 # App title
 st.title("🏨 Neovis external bot assistant")
